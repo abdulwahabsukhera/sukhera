282389d7953941fadf7570727506fb323b73209f
abdulwahabsukhera <2024f-mulug-0509@mul.edu.pk>
2026-06-12 16:28:09 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,49 @@
+```javascript
+const apiKey = "YOUR_API_KEY";
+
+async function getWeather() {
+
+    const city = document.getElementById("cityInput").value;
+
+    if(city === ""){
+        alert("Please enter a city name");
+        return;
+    }
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    try{
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if(data.cod == "404"){
+            alert("City not found");
+            return;
+        }
+
+        document.getElementById("cityName").innerText = data.name;
+
+        document.getElementById("temperature").innerText =
+            `Temperature: ${data.main.temp} °C`;
+
+        document.getElementById("description").innerText =
+            data.weather[0].description;
+
+        document.getElementById("humidity").innerText =
+            `Humidity: ${data.main.humidity}%`;
+
+        document.getElementById("wind").innerText =
+            `Wind Speed: ${data.wind.speed} km/h`;
+
+        const icon = data.weather[0].icon;
+
+        document.getElementById("weatherIcon").src =
+            `https://openweathermap.org/img/wn/${icon}@2x.png`;
+
+    }
+    catch(error){
+        alert("Something went wrong");
+    }
+}
+```
